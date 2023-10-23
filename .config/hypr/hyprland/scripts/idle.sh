1b6d8829d102147cb602d93e00acbf2b73e21f31
Ahmed AbouEleyoun <amedoeyes@gmail.com>
2023-10-23 19:21:46 +0300

    update hypr

--- .config/hypr/hyprland/scripts/idle.sh
+++ .config/hypr/hyprland/scripts/idle.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 swayidle -w \
+	timeout 300 'brightnessctl -s s 5%' \
+	resume 'brightnessctl -r' \
 	timeout 600 'swaylock -f' \
 	timeout 600 'hyprctl dispatch dpms off' \
 	resume 'hyprctl dispatch dpms on' \
+	timeout 900 'systemctl suspend' \
 	before-sleep 'swaylock - f'
