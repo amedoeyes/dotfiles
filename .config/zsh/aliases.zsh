c3ea56767a131ae6e792ff219ea887db52aa8438
Ahmed AbouEleyoun <amedoeyes@gmail.com>
2023-10-27 23:35:50 +0200

    update zsh

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -1,7 +1,5 @@
-alias update="sudo pacman -Syyu --noconfirm && yay -Su -a --noconfirm"
-alias pacman="sudo pacman"
-alias wget="wget --hsts-file='\$XDG_DATA_HOME/wget-hsts'"
-alias cat="bat"
+alias cat="bat --color always"
+alias cfg='git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 alias ls="exa --icons --color=never"
 alias nv="nvim"
-alias config='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+alias pm="paru"
--- .config/zsh/variables.zsh
+++ .config/zsh/variables.zsh
@@ -2,11 +2,11 @@ export PATH=$PATH:$XDG_BIN_HOME:$COMPOSER_HOME
 export EDITOR=nvim
 export VISUAL=nvim
 export BROWSER=librewolf
-export TERM=alacritty
+export TERMINAL=alacritty
 export PAGER=less
 
-
 export PS1="$ "
+export SUDO_PROMPT="Password: "
 export HISTSIZE=100000
 export SAVEHIST=$HISTSIZE
 
@@ -16,18 +16,20 @@ export XDG_DATA_HOME=$HOME/.local/share
 export XDG_STATE_HOME=$HOME/.local/state
 export XDG_BIN_HOME=$HOME/.local/bin
 
+export COMPOSER_HOME=$XDG_CONFIG_HOME/composer/vendor/bin
+export GTK2_RC_FILES=$XDG_CONFIG_HOME/gtk-2.0/gtkrc
+export NPM_CONFIG_USERCONFIG=$XDG_CONFIG_HOME/npm/npmrc
+
 export ANDROID_HOME=$XDG_DATA_HOME/android
 export CARGO_HOME=$XDG_DATA_HOME/cargo
-export COMPOSER_HOME=$XDG_CONFIG_HOME/composer/vendor/bin
 export GNUPGHOME=$XDG_DATA_HOME/gnupg
 export GOPATH="$XDG_DATA_HOME"/go
-export GTK2_RC_FILES=$XDG_CONFIG_HOME/gtk-2.0/gtkrc
-export HISTFILE=$XDG_STATE_HOME/zsh/history
-export LESSHISTFILE=$XDG_CACHE_HOME/less/history
-export MYSQL_HISTFILE=$XDG_DATA_HOME/mysql_history
-export NODE_REPL_HISTORY=$XDG_DATA_HOME/node_repl_history
-export NPM_CONFIG_USERCONFIG=$XDG_CONFIG_HOME/npm/npmrc
 export WINEPREFIX=$XDG_DATA_HOME/wine
+
+export HISTFILE=$XDG_CACHE_HOME/zsh/history
+export LESSHISTFILE=$XDG_CACHE_HOME/less/history
+export MYSQL_HISTFILE=$XDG_CACHE_HOME/mysql_history
+export NODE_REPL_HISTORY=$XDG_CACHE_HOME/node_repl_history
 
 export ZVM_VI_HIGHLIGHT_FOREGROUND=#a0a0a0
 export ZVM_VI_HIGHLIGHT_BACKGROUND=#202020
