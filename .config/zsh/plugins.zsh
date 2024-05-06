3863d82cd9eecd429b146f384214614fb8166682
Ahmed AbouEleyoun <amedoeyes@gmail.com>
2024-05-06 18:53:24 +0300

    (zsh): remove syntax highlighting

--- .config/zsh/plugins.zsh
+++ .config/zsh/plugins.zsh
@@ -1,7 +1,6 @@
 source /usr/share/zsh/scripts/zplug/init.zsh
 
 zplug "zsh-users/zsh-autosuggestions"
-zplug "zsh-users/zsh-syntax-highlighting"
 zplug "jeffreytse/zsh-vi-mode"
 zplug "junegunn/fzf", use:"shell/*.zsh"
 
