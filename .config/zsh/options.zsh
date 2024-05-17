63235815b2d21a4d72aab83c1b2f81cb98cc828f
Ahmed AbouEleyoun <amedoeyes@gmail.com>
2024-05-17 17:47:52 +0300

    (zsh): add new options

--- .config/zsh/options.zsh
+++ .config/zsh/options.zsh
@@ -1,16 +1,20 @@
-setopt EXTENDED_HISTORY
-setopt HIST_EXPIRE_DUPS_FIRST
-setopt HIST_FIND_NO_DUPS
-setopt HIST_IGNORE_ALL_DUPS
-setopt HIST_IGNORE_DUPS
-setopt HIST_IGNORE_SPACE
-setopt HIST_SAVE_NO_DUPS
-setopt SHARE_HISTORY
+setopt extended_history
+setopt hist_expire_dups_first
+setopt hist_find_no_dups
+setopt hist_ignore_all_dups
+setopt hist_ignore_dups
+setopt hist_ignore_space
+setopt hist_save_no_dups
+setopt share_history
+setopt append_history
 
 autoload -Uz compinit
 compinit -d "$XDG_CACHE_HOME"/zsh/zcompdump-"$ZSH_VERSION"
 eval "$(zoxide init zsh --cmd cd)"
 
-zstyle ':completion:*' menu select
+zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
+zstyle ':completion:*' menu no
 zstyle ':completion::complete:*' gain-privileges 1
+zstyle ':completion:*' use-cache on
 zstyle ':completion:*' cache-path "$XDG_CACHE_HOME"/zsh/zcompcache
+zstyle ':fzf-tab:complete:cd:*' fzf-preview 'ls $realpath'
--- .config/zsh/variables.zsh
+++ .config/zsh/variables.zsh
@@ -8,6 +8,7 @@ export READER=zathura
 
 export PS1="$ "
 export SUDO_PROMPT="Password: "
+
 export HISTSIZE=100000
 export SAVEHIST=$HISTSIZE
 
@@ -31,7 +32,7 @@ export WINEPREFIX=$XDG_DATA_HOME/wine
 export XCURSOR_PATH=$XDG_DATA_HOME/icons
 export ZINIT_HOME=$XDG_DATA_HOME/zinit/zinit.git
 
-export FZF_DEFAULT_OPTS="--ansi --color=bw"
+export FZF_DEFAULT_OPTS="--ansi --color='fg:#A0A0A0,fg+:#A0A0A0,bg:#000000,bg+:#101010,hl:#A0A0A0,hl+:#A0A0A0,gutter:#000000,info:#707070,border:#404040,prompt:#A0A0A0,pointer:#A0A0A0,marker:#A0A0A0,spinner:#A0A0A0' --height 50% --reverse --border --inline-info --prompt='> '"
 export FZF_ALT_C_COMMAND="fd -t d --exclude node_modules --exclude old ."
 export FZF_ALT_C_OPTS="--height 100%"
 export FZF_CTRL_T_COMMAND="fd --exclude node_modules --exclude old ."
@@ -41,4 +42,4 @@ export ZVM_INIT_MODE=sourcing
 export ZVM_VI_HIGHLIGHT_FOREGROUND=#a0a0a0
 export ZVM_VI_HIGHLIGHT_BACKGROUND=#202020
 
-ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=#404040'
+export ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=#404040'
