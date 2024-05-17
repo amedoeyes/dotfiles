b8cc562d7f7392e8238ae2f7d34e35f4e0eec862
Ahmed AbouEleyoun <amedoeyes@gmail.com>
2024-05-17 17:47:15 +0300

    (zsh): replace zplug with zinit

--- .config/zsh/plugins.zsh
+++ .config/zsh/plugins.zsh
@@ -1,7 +1,15 @@
-source /usr/share/zsh/scripts/zplug/init.zsh
+if [ ! -d "$ZINIT_HOME" ]; then
+	mkdir -p "$(dirname $ZINIT_HOME)"
+	git clone https://github.com/zdharma-continuum/zinit.git "$ZINIT_HOME" --depth=1
+fi
 
-zplug "zsh-users/zsh-autosuggestions"
-zplug "jeffreytse/zsh-vi-mode"
-zplug "junegunn/fzf", use:"shell/*.zsh"
+source "$ZINIT_HOME/zinit.zsh"
 
-zplug load
+autoload -Uz _zinit
+(( ${+_comps} )) && _comps[zinit]=_zinit
+
+zinit ice depth=1; zinit load "zsh-users/zsh-autosuggestions"
+zinit ice depth=1; zinit load "zsh-users/zsh-completions"
+zinit ice depth=1; zinit load "jeffreytse/zsh-vi-mode"
+zinit ice depth=1; zinit light "junegunn/fzf"; eval "$(fzf --zsh)"
+zinit ice depth=1; zinit light "Aloxaf/fzf-tab"
