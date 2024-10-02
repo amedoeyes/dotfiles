5786bfb6d47f150d07d95b3a6e13135c70ecfdee
Ahmed AbouEleyoun <amedoeyes@gmail.com>
2024-10-02 23:33:48 +0300

    (zsh): alias nvim to prevent nesting

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -8,3 +8,7 @@ alias py="python -q"
 alias zathura="zathura --fork"
 alias adb="HOME="$XDG_DATA_HOME"/android adb"
 alias fd="fd --color=never"
+
+if [[ -n "$NVIM" ]]; then
+	alias nvim="nvim --server $NVIM --remote"
+fi
