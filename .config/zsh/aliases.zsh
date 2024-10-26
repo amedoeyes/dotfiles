5024b26583195ab29a4376e4f91e167160353c4e
Ahmed AbouEleyoun <amedoeyes@gmail.com>
2024-10-26 18:38:45 +0300

    zsh: remove the nvim remote alias

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -8,7 +8,3 @@ alias py="python -q"
 alias zathura="zathura --fork"
 alias adb="HOME="$XDG_DATA_HOME"/android adb"
 alias fd="fd --color=never"
-
-if [[ -n "$NVIM" ]]; then
-	alias nvim="nvim --server $NVIM --remote"
-fi
