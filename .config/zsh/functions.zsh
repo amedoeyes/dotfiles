003c372abf66695a1c28a449fddf4d189e3f87b2
Ahmed AbouEleyoun <amedoeyes@gmail.com>
2025-03-27 16:00:35 +0200

    scripts: move zsh functions to standalone scripts

--- .config/zsh/functions.zsh
+++ .config/zsh/functions.zsh
@@ -1,36 +1,11 @@
+autoload -Uz add-zsh-hook
+
+function cht.sh {
+	curl -s "cheat.sh/$1?qT"
+}
+
 function spell {
 	echo $1 | hunspell -a | grep "^&" | awk -F": " '{print $2}'
-}
-
-function screenshot {
-	local directory="$HOME/pictures/screenshots/$(date +%Y)/$(date +%m)/$(date +%d)/"
-	local -r filename=$(date +%H%M%S)
-	local format="png"
-	local file="$directory/$filename.$format"
-	[ ! -d $directory ] && mkdir -p $directory
-	if [[ -n $1 && $1 == "-s" ]]; then
-		local -r geometry=$(select-geometry)
-		if [[ -n $geometry ]]; then
-			sleep 0.2
-			grim -g $geometry $file
-			wl-copy -t image/png <$file
-		fi
-	else
-		grim $file
-	fi
-}
-
-function screenrecord {
-	local directory="$HOME/videos/screenrecords/$(date +%Y)/$(date +%m)/$(date +%d)/"
-	local -r filename=$(date +%H%M%S)
-	local format="mp4"
-	local file="$directory/$filename.$format"
-	[ ! -d $directory ] && mkdir -p $directory
-	wf-recorder -f $file $@
-}
-
-function select-geometry {
-	swaymsg -t get_tree | jq -r '.. | select(.pid? and .visible?) | .rect | "\(.x),\(.y) \(.width)x\(.height)"' | slurp -b "#000000A0" -c "#A0A0A0FF" -s "#00000000" -B "#000000A0" -w 1 -o
 }
 
 function vifm {
@@ -38,6 +13,7 @@ function vifm {
 	[ -z $dst ] && return 1
 	cd $dst
 }
+
 
 function osc7-pwd {
 	emulate -L zsh 
