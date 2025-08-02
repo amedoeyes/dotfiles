41c68dd788ccab0c233913ab8ec3d22b28fecf3f
Ahmed AbouEleyoun <amedoeyes@gmail.com>
2025-08-03 02:05:36 +0300

    fix: screenrecord script

--- pkgs/screenrecord.nix
+++ pkgs/screenrecord.nix
@@ -4,7 +4,7 @@
   wf-recorder,
 }:
 writeShellScriptBin "screenrecord" ''
-  file="$XDG_VIDEOS_DIR/screenshots/$(date +%Y/%m/%d/%H%M%S).png" 
+  file="$XDG_VIDEOS_DIR/screenrecords/$(date +%Y/%m/%d/%H%M%S).mp4"
   mkdir -p "$(dirname $file)" 
   ${lib.getExe wf-recorder} -f "$file" "$@"
 ''
