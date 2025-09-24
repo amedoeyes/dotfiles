1473403c2744203552fa8ffa706a178b5a6285a5
Ahmed AbouEleyoun <amedoeyes@gmail.com>
2025-09-24 20:09:43 +0300

    feat: use 0 pading for numbering in cmus

--- home/programs/cmus.nix
+++ home/programs/cmus.nix
@@ -76,6 +76,11 @@ in
       theme = "eyes";
 
       extraConfig = ''
+        set format_current= %a - %l%! - %02n %t%= %y 
+        set format_trackwin= %02n %t%= %d 
+        set format_playlist= %-21%a %02n %t%= %y %d %{?X!=0?%3X ?    }
+        set format_playlist_va= %-21%A %02n %t (%a)%= %y %d %{?X!=0?%3X ?    }
+        set format_trackwin_va= %02n %t (%a)%= %d 
         set status_display_program=cmusfm
       '';
     };
