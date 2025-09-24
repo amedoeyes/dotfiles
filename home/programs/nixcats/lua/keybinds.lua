1e60a66fa52a91ef1690445eec2a81e40e881040
Ahmed AbouEleyoun <amedoeyes@gmail.com>
2025-09-24 20:30:45 +0300

    feat: neovim remove redundant keybind

--- home/programs/nixcats/lua/keybinds.lua
+++ home/programs/nixcats/lua/keybinds.lua
@@ -1,7 +1,6 @@
 vim.keymap.set({ "n", "x" }, "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 vim.keymap.set({ "n", "x" }, "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 
-vim.keymap.set("n", "<esc>", "<cmd>noh<cr><esc>")
 vim.keymap.set("n", "n", "'Nn'[v:searchforward].'zv'", { expr = true })
 vim.keymap.set("n", "N", "'nN'[v:searchforward].'zv'", { expr = true })
 vim.keymap.set({ "x", "o" }, "n", "'Nn'[v:searchforward]", { expr = true })
