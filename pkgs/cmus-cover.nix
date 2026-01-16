cfd762e3fb7371d60194b2082e139ba1309d8406
Ahmed AbouEleyoun <amedoeyes@gmail.com>
2026-01-16 16:37:22 +0200

    feat(pkgs/cmus-cover): handle WINCH

--- pkgs/cmus-cover.nix
+++ pkgs/cmus-cover.nix
@@ -7,18 +7,40 @@
 }:
 writeShellScriptBin "cmus-cover" ''
   function display_cover {
-  	${lib.getExe ffmpeg} -loglevel quiet -i "$(${lib.getExe' cmus "cmus-remote"} -Q | grep 'file' | cut -d ' ' -f 2-)" -an -vcodec copy -f image2pipe - |
-  		${lib.getExe chafa} -s "$(tput cols)"x"$(tput lines)" --align "center,center" --clear |
-  		tr -d '\n'
-  	echo -en "\e[?25l"
+    ${lib.getExe ffmpeg} -loglevel quiet -i "$(${lib.getExe' cmus "cmus-remote"} -Q | grep 'file' | cut -d ' ' -f 2-)" -an -vcodec copy -f image2pipe - |
+      ${lib.getExe chafa} -s "$(tput cols)"x"$(tput lines)" --align "center,center" --clear |
+      tr -d '\n'
+    tput civis
   }
 
-  display_cover
+  cleanup() {
+  	kill "$COPROC_PID" 2>/dev/null
+  	tput rmcup
+  	tput cnorm
+  	exit
+  }
 
-  dbus-monitor "type='signal',interface='org.freedesktop.DBus.Properties',member='PropertiesChanged'" |
-  	while read -r line; do
-  		if echo "$line" | grep -q "Metadata"; then
-  			display_cover
+  need_redraw=1
+
+  trap 'need_redraw=1' WINCH
+  trap cleanup INT TERM EXIT
+
+  coproc {
+  	exec dbus-monitor "type='signal',interface='org.freedesktop.DBus.Properties',member='PropertiesChanged'"
+  }
+
+  tput smcup
+
+  while :; do
+  	if read -r -t 0.1 line <&"''${COPROC[0]}"; then
+  		if [[ $line == *Metadata* ]]; then
+  			need_redraw=1
   		fi
-  	done
+  	fi
+
+  	if ((need_redraw == 1)); then
+  		display_cover
+  		need_redraw=0
+  	fi
+  done
 ''
