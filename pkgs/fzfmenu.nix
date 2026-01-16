d6febac724f11e5608837ccaf1a0a93522ea1835
Ahmed AbouEleyoun <amedoeyes@gmail.com>
2026-01-16 16:40:09 +0200

    refactor(pkgs/fzfmenu): use flock and sh directly

--- pkgs/fzfmenu.nix
+++ pkgs/fzfmenu.nix
@@ -1,14 +1,12 @@
 {
-  stdenv,
   writeShellScriptBin,
   lib,
-  flock,
   fzf,
   terminalCmd ? "",
   fzfOptions ? [ ],
 }:
 writeShellScriptBin "fzfmenu" ''
   exec 9>"$XDG_RUNTIME_DIR/fzfmenu.lock"
-  ${lib.getExe flock} -n 9 || exit
-  ${terminalCmd} ${stdenv.shell} -c "echo -ne '\\e[5 q' && ${lib.getExe fzf} $FZF_DEFAULT_OPTS --margin 1 --separator='─' ${lib.strings.concatStringsSep " " fzfOptions} $* </proc/$$/fd/0 >/proc/$$/fd/1"
+  flock -n 9 || exit
+  ${terminalCmd} sh -c "echo -ne '\\e[5 q' && ${lib.getExe fzf} $FZF_DEFAULT_OPTS --margin 1 --separator='─' ${lib.strings.concatStringsSep " " fzfOptions} $* </proc/$$/fd/0 >/proc/$$/fd/1"
 ''
