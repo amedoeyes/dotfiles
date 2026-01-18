854e5b8ab5eec2475fb13d8a44ee581aed12e241
Ahmed AbouEleyoun <amedoeyes@gmail.com>
2026-01-18 10:42:07 +0200

    feat(nvim): update efm lsp config

--- home/programs/nixcats/lsp/efm-langserver.lua
+++ home/programs/nixcats/lsp/efm-langserver.lua
@@ -67,8 +67,8 @@ return {
 			yaml = { prettier("yaml") },
 		},
 	},
-	on_init = function(client)
-		client.server_capabilities.documentFormattingProvider = true
-		client.server_capabilities.documentRangeFormattingProvider = true
-	end,
+	init_options = {
+		documentFormatting = true,
+		documentRangeFormatting = true,
+	},
 }
