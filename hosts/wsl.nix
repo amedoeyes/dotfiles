83e7493f2e8f81ab36ba46a91cbea5548248d926
Ahmed AbouEleyoun <amedoeyes@gmail.com>
2026-01-26 01:09:48 +0200

    refactor: inline wsl user in wsl host

--- hosts/wsl.nix
+++ hosts/wsl.nix
@@ -4,7 +4,52 @@
     {
       modules = [ inputs.nixos-wsl.nixosModules.default ];
       users = {
-        wsl = import ../users/wsl.nix;
+        wsl = {
+          user =
+            { pkgs, ... }:
+            {
+              shell = pkgs.fish;
+              isNormalUser = true;
+              extraGroups = [ "wheel" ];
+            };
+
+          home =
+            { pkgs, lib, ... }:
+            {
+              home.packages = with pkgs; [
+                nix-index
+                wl-clipboard
+                xdg-utils
+                spell
+              ];
+
+              xdg.portal.enable = lib.mkForce false;
+
+              programs = {
+                btop.enable = true;
+                dircolors.enable = true;
+                direnv.enable = true;
+                fish.enable = true;
+                fzf.enable = true;
+                git = {
+                  enable = true;
+                  settings = {
+                    user = {
+                      email = "ahmed.aboeleyoun@trufla.com";
+                      name = "Ahmed AbouEleyoun";
+                    };
+                  };
+                };
+                helix = {
+                  enable = true;
+                  default = true;
+                };
+                lazygit.enable = true;
+                yazi.enable = true;
+                zk.enable = true;
+              };
+            };
+        };
       };
       monitors = [ ];
     };
