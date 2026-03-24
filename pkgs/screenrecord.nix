8d9fb120ad43f003ee80e56cb6a3cef399346d31
Ahmed AbouEleyoun <amedoeyes@gmail.com>
2026-03-24 10:02:21 +0200

    feat(pkgs): quote $file in screenshot and screenrecord

--- pkgs/screenrecord.nix
+++ pkgs/screenrecord.nix
@@ -5,6 +5,6 @@
 }:
 writeShellScriptBin "screenrecord" ''
   file="$XDG_VIDEOS_DIR/screenrecords/$(date +%Y/%m/%d/%H%M%S).mp4"
-  mkdir -p "$(dirname $file)" 
+  mkdir -p "$(dirname "$file")"
   ${lib.getExe wf-recorder} -f "$file" "$@"
 ''
--- pkgs/screenshot.nix
+++ pkgs/screenshot.nix
@@ -5,8 +5,8 @@
   wl-clipboard-rs,
 }:
 writeShellScriptBin "screenshot" ''
-  file="$XDG_PICTURES_DIR/screenshots/$(date +%Y/%m/%d/%H%M%S).png" 
-  mkdir -p "$(dirname $file)" 
+  file="$XDG_PICTURES_DIR/screenshots/$(date +%Y/%m/%d/%H%M%S).png"
+  mkdir -p "$(dirname "$file")"
   ${lib.getExe grim} "$@" "$file"
   ${lib.getExe' wl-clipboard-rs "wl-copy"} -t image/png <"$file"
 ''
