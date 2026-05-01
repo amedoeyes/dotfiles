7db95b2fad6b0619c3c80cf095c0eb6d224ba49d
Ahmed AbouEleyoun <amedoeyes@gmail.com>
2026-05-01 05:02:19 +0300

    fix(pkgs/newsboat): background color

--- home/programs/newsboat.nix
+++ home/programs/newsboat.nix
@@ -11,19 +11,19 @@ in
       bind-key j down
       bind-key k up
 
-      color background          color${c10.ansi} color${c10.ansi}
-      color listnormal          color${c10.ansi} color${c10.ansi}
+      color background          color${c10.ansi} color${c00.ansi}
+      color listnormal          color${c10.ansi} color${c00.ansi}
       color listfocus           color${c10.ansi} color${c01.ansi}
-      color listnormal_unread   color${c10.ansi} color${c10.ansi}
+      color listnormal_unread   color${c10.ansi} color${c00.ansi}
       color listfocus_unread    color${c10.ansi} color${c01.ansi}
-      color title               color${c10.ansi} color${c10.ansi} bold
-      color info                color${c10.ansi} color${c10.ansi} bold
-      color hint-key            color${c10.ansi} color${c10.ansi}
-      color hint-keys-delimiter color${c10.ansi} color${c10.ansi}
-      color hint-separator      color${c10.ansi} color${c10.ansi}
-      color hint-description    color${c10.ansi} color${c10.ansi}
-      color article             color${c10.ansi} color${c10.ansi}
-      color end-of-text-marker  color${c10.ansi} color${c10.ansi}
+      color title               color${c10.ansi} color${c00.ansi} bold
+      color info                color${c10.ansi} color${c00.ansi} bold
+      color hint-key            color${c10.ansi} color${c00.ansi}
+      color hint-keys-delimiter color${c10.ansi} color${c00.ansi}
+      color hint-separator      color${c10.ansi} color${c00.ansi}
+      color hint-description    color${c10.ansi} color${c00.ansi}
+      color article             color${c10.ansi} color${c00.ansi}
+      color end-of-text-marker  color${c10.ansi} color${c00.ansi}
 
       highlight article "https?://[^ ]+" color${c08.ansi} default underline
     '';
